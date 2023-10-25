cbec0529453e2efa7c98162de8b89827a78531e9
Lukas Mateffy <github@mateffy.me>
2023-10-25 14:07:33 +0200

    v1 release

--- domos-core.php
+++ domos-core.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name:     DOMOS Core
+ * Plugin Name:     domos für WordPress
  * Plugin URI:      https://domos.de
  * Description:     Die domos WordPress-Integration
- * Version:         0.0.5
+ * Version:         1.0.0
  * Author:          domos GmbH
  * Author URI:      https://domos.de
  * Text Domain:     domos
@@ -14,7 +14,7 @@ require_once __DIR__.'/vendor/autoload.php';
 define('DOMOS_CORE_ROOT', __DIR__);
 define('DOMOS_CORE_ROOT_FILE', __FILE__);
 define('DOMOS_CORE_URL', plugin_dir_url(__FILE__));
-define('DOMOS_CORE_VERSION', '0.0.5');
+define('DOMOS_CORE_VERSION', '1.0.0');
 define('DOMOS_CORE_SLUG', 'domos-core');
 
 $plugin = new Domos\Core\Providers\DomosCoreServiceProvider;
