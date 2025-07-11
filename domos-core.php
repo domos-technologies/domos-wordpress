272b4b1c08de2a0f72de0939c2514cbab34050bc
Lukas Mateffy <github@mateffy.me>
2025-07-11 15:38:08 +0200

    chore: update version to 3.0.0

--- domos-core.php
+++ domos-core.php
@@ -14,7 +14,7 @@ require_once __DIR__.'/vendor/autoload.php';
 define('DOMOS_CORE_ROOT', __DIR__);
 define('DOMOS_CORE_ROOT_FILE', __FILE__);
 define('DOMOS_CORE_URL', plugin_dir_url(__FILE__));
-define('DOMOS_CORE_VERSION', '2.0.3');
+define('DOMOS_CORE_VERSION', '3.0.0');
 define('DOMOS_CORE_SLUG', 'domos-core');
 
 $plugin = new Domos\Core\Providers\DomosCoreServiceProvider;
